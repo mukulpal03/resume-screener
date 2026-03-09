6019036719d80bb35e894b09a2ccbd900c167f0c
Mukul Pal <mukulpal0333@gmail.com>
2026-03-09 13:10:05 +0530

    feat: add db setup with drizzle

---
+++ packages/db/src/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pkg from 'pg';
+import * as schema from './schema';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
+
+export * from './schema';
