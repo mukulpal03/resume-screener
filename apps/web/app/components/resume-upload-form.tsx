3ff96064c0fd672943693594e1dbcbb290b582ab
praveen <palpraveen3221@gmail.com>
2026-03-14 10:00:49 +0530

    feat: implement react-hook form

---
+++ apps/web/app/components/resume-upload-form.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { UploadResumeCard, JDTextarea, AppButton } from '@repo/ui';
+import { uploadResume } from '.././services/resume.service';
+import { validateResumeFlow } from '.././lib/resume-validation';
+import { toast } from '@repo/ui';
+import { useRouter } from 'next/navigation';
+
+type FormValues = {
+  resume: File | null;
+  jobDescription: string;
+};
+
+export default function ResumeUploadForm() {
+  const router = useRouter();
+
+  const { handleSubmit, setValue, watch } = useForm<FormValues>({
+    defaultValues: {
+      resume: null,
+      jobDescription: '',
+    },
+  });
+
+  const resumeFile = watch('resume');
+  const jobDescription = watch('jobDescription');
+
+  const handleFileupload = async (file: File) => {
+    setValue('resume', file);
+  };
+
+  const onSubmit = async () => {
+    const error = validateResumeFlow(resumeFile, jobDescription);
+
+    if (error) {
+      toast.error(error);
+      return;
+    }
+
+    try {
+      if (resumeFile) {
+        await uploadResume(resumeFile, jobDescription);
+        toast.success('Resume Parsed Successfully');
+        router.push('/results');
+      }
+    } catch {
+      toast.error('Failed to upload resume');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <UploadResumeCard onUpload={handleFileupload} />
+
+        <JDTextarea
+          value={jobDescription}
+          onChange={(value) => setValue('jobDescription', value)}
+        />
+      </div>
+
+      <div className="mt-10 flex justify-center">
+        <AppButton type="submit" size="md">
+          Analyze Resume
+        </AppButton>
+      </div>
+    </form>
+  );
+}
