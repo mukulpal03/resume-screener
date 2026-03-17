6de47264311139b52cbcaa2eae2181f0e08fa5b7
praveen <palpraveen3221@gmail.com>
2026-03-17 18:04:59 +0530

    refactor: move types to @repo/types package

---
+++ packages/types/src/result.ts
@@ -0,0 +1,9 @@
+export interface Breakdown {
+  skills_match: number;
+  experience_relevance: number;
+  education: number;
+}
+
+export interface ResultBreakdownProps {
+  breakdown: Breakdown;
+}
