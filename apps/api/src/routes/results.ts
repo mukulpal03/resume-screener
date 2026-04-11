566401872a40709e9c48682e91149529fe1c1bcf
Mukul Pal <mukulpal0333@gmail.com>
2026-04-11 11:26:58 +0530

    feat: add an endpoint to fetch the single result

--- apps/api/src/routes/results.ts
+++ apps/api/src/routes/results.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getResultsHistory } from '../controllers/results';
+import { getResultById, getResultsHistory } from '../controllers/results';
 
 const router: Router = Router();
 
 router.get('/history', getResultsHistory);
+router.get('/:id', getResultById);
 
 export default router;
